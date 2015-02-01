5441c18a67c4e6db26416486c2b5c73f1955081c
Garikk <supergarikk@gmail.com>
2015-02-01 21:18:00 +0400

    Fix UUID

--- src/kkdev/kksystem/plugin/leddisplay/LEDPluginInfo.java
+++ src/kkdev/kksystem/plugin/leddisplay/LEDPluginInfo.java
@@ -18,6 +18,7 @@ public final class LEDPluginInfo  {
     {
         PluginInfo Ret=new PluginInfo();
         
+        Ret.PluginUUID="7fbac0f7-6939-4380-bcb0-0ef8b1580fbf";
         Ret.PluginName="KKODB2Reader";
         Ret.PluginDescription="Basic ELM327 ODB2 Reader plugin";
         Ret.PluginType = KK_PLUGIN_TYPE.PLUGIN_INPUT;
@@ -31,7 +32,7 @@ public final class LEDPluginInfo  {
     
     private static String[] GetMyReceivePinInfo(){
     
-        String[] Ret=new String[1];
+        String[] Ret=new String[4];
     
         Ret[0]=PluginConsts.KK_PLUGIN_BASE_PLUGIN_DEF_PIN_COMMAND;
         Ret[1]=PluginConsts.KK_PLUGIN_BASE_PLUGIN_DEF_PIN_LED_COMMAND;
@@ -42,7 +43,7 @@ public final class LEDPluginInfo  {
     }
     private static String[] GetMyTransmitPinInfo(){
     
-        String[] Ret=new String[1];
+        String[] Ret=new String[2];
     
         Ret[0]=PluginConsts.KK_PLUGIN_BASE_PLUGIN_DEF_PIN_LED_DATA;
         Ret[1]=PluginConsts.KK_PLUGIN_BASE_PLUGIN_DEF_PIN_LED_RAW;
